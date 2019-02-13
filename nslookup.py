9ac62c6c9e53197ef0db8d236a5b899d79c439d9
rkayutkin <41807279+rkayutkin@users.noreply.github.com>
2019-02-13 12:18:15 -0800

    This is the working dig query from a file that grabs IPs and sorts them into a dictionary.

--- nslookup.py
+++ nslookup.py
@@ -2,13 +2,25 @@ import subprocess
 
 hfile= open("hostnames.txt", "r")
 
-main= {'key':'value'}
+main= {}
 
 for i in hfile:
+    # remove any ending spaces with srrip for the elements
+    hfile2= i.strip()
+    # parse for column 5
     grep1= " | awk '{print $5}'"
-    grep2= " | grep -P '\b([0-9]{1,3}\.){3}[0-9]{1,3}\b'"
-    lookup= 'nslookup ' + i + grep1
-    output= subprocess.check_output(lookup, shell=True)
-    print (output)
+    # Regex i managed to get working to parse the IP addresses
+    grep2= " | grep -E '\d.*[0-9]\d.*.'"
+    # concatenating to build the command.
+    lookup= "dig " + hfile2 + grep1 + grep2
+    # Running the command with subprocess using shell, not safe.
+    output= subprocess.check_output(lookup,shell=True)
+    # Add a " , " between the output and create a new variable 
+    output2= " , ".join(output.split())
+    # Add the output from hile2 to Key and output2 to Value
+    main[hfile2]= [output2]
+
+print main
 
 hfile.close()
+
