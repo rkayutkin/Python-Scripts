e54aa3034182bc05c210c255b16e2348c173e3b2
Roman Kayutkin <rkayutkin@P12E-MBR13-060.local>
2019-05-07 14:41:08 -0700

    Removed Proprietary Information

--- interface.py
+++ interface.py
@@ -4,20 +4,13 @@ os.system('clear')
 
 print ("Creating Configuration:\n")
 
-# [(x, y) for x in range(325,349) for y in range (1,25)]
-#     print ('int po ' + str(x))
-#     print ('description xcoder' + str(y))
-
-# for x in range (1,25):
-#     description= ('description xcoder' + str(x) + '.c01.xcode.file.clt2.prod')
-
 count=1
 
 print ("######Configuration for LEAF1######:\n")
 
 for n in range (325,349):
     print ('int po ' + str(n))
-    print ('description xcoder' + str(count) + '.c01.xcode.file.clt2.prod')
+    print ('description xcoder' + str(count) + '.xxxx.xxxx')
     print ('switchport')
     print ('switchport access vlan 3001')
     print ('spanning-tree port type edge')
@@ -33,7 +26,7 @@ count3=325
 
 for i in range (25,49):
     print ('int eth1/' + str(i))
-    print ('description xcoder' + str(count2) + '.c01.xcode.file.clt2.prod-LAN-1')
+    print ('description xcoder' + str(count2) + '.xxxx.xxxx')
     print ('switchport')
     print ('switchport access vlan 3001')
     print ('spanning-tree port type edge')
@@ -48,7 +41,7 @@ count4=1
 
 for j in range (325,349):
     print ('int po ' + str(j))
-    print ('description xcoder' + str(count4) + '.c01.xcode.file.clt2.prod')
+    print ('description xcoder' + str(count4) + '.xxxx.xxxx')
     print ('switchport')
     print ('switchport access vlan 3001')
     print ('spanning-tree port type edge')
@@ -63,7 +56,7 @@ count6=325
 
 for k in range (25,49):
     print ('int eth1/' + str(k))
-    print ('description xcoder' + str(count5) + '.c01.xcode.file.clt2.prod-LAN-2')
+    print ('description xcoder' + str(count5) + '.xxxx.xxxx')
     print ('switchport')
     print ('switchport access vlan 3001')
     print ('spanning-tree port type edge')
--- interfaceoob.py
+++ interfaceoob.py
@@ -4,20 +4,13 @@ os.system('clear')
 
 print ("Creating Configuration:\n")
 
-# [(x, y) for x in range(325,349) for y in range (1,25)]
-#     print ('int po ' + str(x))
-#     print ('description xcoder' + str(y))
-
-# for x in range (1,25):
-#     description= ('description xcoder' + str(x) + '.c01.xcode.file.clt2.prod')
-
 print ("OSW1J03.ewr1\n")
 
 count=1
 
 for i in range (25,49):
     print ('int Gi0/' + str(i))
-    print ('description xcoder' + str(count) + '.c01.xcode.file.clt2.prod-OOB')
+    print ('description xcoder' + str(count) + '.xxxx.xxxx')
     print ('switchport access vlan 306')
     print ('\n')
     count+=1
@@ -28,7 +21,7 @@ count2=25
 
 for x in range (1,12):
     print ('int Gi0/' + str(x))
-    print ('description xcoder' + str(count2) + '.c01.xcode.file.clt2.prod-OOB')
+    print ('description xcoder' + str(count2) + '.xxxx.xxxx')
     print ('switchport access vlan 306')
     print ('\n')
     count2+=1
